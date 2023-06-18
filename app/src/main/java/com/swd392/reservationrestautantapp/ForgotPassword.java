d5e2ee7844d35ed5555c7a947db4362864a5a8c4
BlueEyesssss <vuamessi9@gmail.com>
2023-06-18 22:55:33 +0700

    done forgot password

---
+++ app/src/main/java/com/swd392/reservationrestautantapp/ForgotPassword.java
@@ -0,0 +1,56 @@
+package com.swd392.reservationrestautantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ForgotPassword extends AppCompatActivity {
+    Button SendButton;
+    TextView loginLink, SignupLink;
+    EditText edt_phone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        findData();
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(ForgotPassword.this, ForgotPassword2.class);
+                //startActivity(intent);
+            }
+        });
+
+        SignupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(ForgotPassword.this, ForgotPassword2.class);
+                //startActivity(intent);
+            }
+        });
+
+        SendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgotPassword.this, ForgotPassword2.class);
+                String phone = edt_phone.getText().toString();
+                intent.putExtra("phone", phone);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void findData() {
+        SendButton = findViewById(R.id.SendButton);
+        loginLink = findViewById(R.id.loginLink);
+        SignupLink = findViewById(R.id.SignupLink);
+        edt_phone = findViewById(R.id.edt_phone);
+    }
+}
---
+++ app/src/main/java/com/swd392/reservationrestautantapp/ForgotPassword2.java
@@ -0,0 +1,36 @@
+package com.swd392.reservationrestautantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ForgotPassword2 extends AppCompatActivity {
+
+    TextView ResendLink, phoneUser;
+    Button BackToLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password2);
+
+        ResendLink = findViewById(R.id.ResendLink);
+        BackToLoginButton = findViewById(R.id.BackToLoginButton);
+        phoneUser = findViewById(R.id.phoneUser);
+
+        Intent intent = getIntent();
+        String phone = intent.getStringExtra("phone");
+        phoneUser.setText(phone);
+
+        BackToLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(ForgotPassword2.class, Login.class));
+            }
+        });
+    }
+}
