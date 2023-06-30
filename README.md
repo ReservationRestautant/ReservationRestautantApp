b78185e698bb5de11afa6e491335a187df1e4587
BlueEyesssss <vuamessi9@gmail.com>
2023-06-30 14:03:07 +0700

    done payment screen

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # RESERVATION RESTAUTANT APP (SWD392)
-- tool: Android Studio - sdk 29
+- tool: Android Studio - sdk 29 - jdk 11
 
 #### © 2023 BlueEyesssss
---
+++ app/src/main/java/com/swd392/reservationrestautantapp/Payment.java
@@ -0,0 +1,39 @@
+package com.swd392.reservationrestautantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Payment extends AppCompatActivity {
+    Button buttonDone;
+    TextView moneypay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+        //find by id
+        buttonDone = findViewById(R.id.buttonDone);
+        moneypay = findViewById(R.id.moneypay);
+
+        Intent intent = getIntent();
+        String moneypaystr = intent.getStringExtra("MONEYPAY");
+        //default data to TEST
+        if(moneypaystr == null) moneypaystr = "1000000";
+
+        //set money pay
+        moneypay.setText("Money to pay: " + moneypaystr + " vnđ");
+
+        buttonDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Payment.this, BookingSuccess.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
